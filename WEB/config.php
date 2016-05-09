48c94eac9faea51de8d9763cde149c4fc06a2aaa
eLGi <ayats.bravo@gmail.com>
2016-05-09 11:33:32 +0200

    Zainstalowanie Laravel
    Dodałem framework PHP - Laravel.

--- WEB/config.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-    define('DB_HOST', '127.0.0.1');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'notesdbb');
-
-    $mySQL = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die("Aplikacja nie mogła połączyć się z bazą danych.");
-
-?>
