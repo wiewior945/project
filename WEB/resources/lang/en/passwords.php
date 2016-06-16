9124c86e67b26af1086eb5ad4c37548d7021f304
eLGi <ayats.bravo@gmail.com>
2016-06-16 19:02:34 +0200

    Konfiguracja maila z przywróceniem hasła

--- WEB/resources/lang/en/passwords.php
+++ WEB/resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Hasło musi mieć przynajmniej sześć znaków.',
+    'reset' => 'Twoje hasło zostało zresetowane!',
+    'sent' => 'Wysłaliśmy wiadomość z odnośnikiem do przywrócenia hasła!',
+    'token' => 'Ten token przywrócenia hasła jest nieprawidłowy.',
+    'user' => "Brak użytkownika o takim adresie w naszej bazie.",
 
 ];
